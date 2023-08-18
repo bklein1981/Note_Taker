81383196bd664848b0d76d1fa7b8aff2a13adcc8
Brian Klein <bklein1981@gmail.com>
2023-08-18 17:16:09 -0500

    Added in additional code for delete route

--- routes/notes.js
+++ routes/notes.js
@@ -5,42 +5,42 @@ const fs = require('fs');
 //Get route for retreiving notes
 notes.get('/', (req, res) => {
     console.log(`route is working`);
+
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.log(err);
-        } else { 
-           return res.json(data);
+        } else {
+            return res.json(JSON.parse(data));
         }
     })
 })
 
 //Post route for displaying notes
 notes.post('/', (req, res) => {
-    
     const { title, text } = req.body;
-
     if (title && text) {
         const newNote = { title, text };
-    
 
-   fs.readFile('./db/db.json', 'utf8', (err, data) => {
-       if (err) {
-           console.log(err);
-       } else { 
-            const noteData = JSON.parse(data);
-            noteData.push(newNote);
-
-            fs.writeFile('./db/db.json', JSON.stringify(noteData), function(err) {
-                console.log('file was saved');
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const noteData = JSON.parse(data);
+                noteData.push(newNote);
+                fs.writeFile('./db/db.json', JSON.stringify(noteData), function (err) {
+                    console.log('file was saved');
+                }
+                );
             }
-            );
-        }
-    });
-    res.status(201);
-} else {
-    res.status(500).json ('Error in posting note');
-}
-    
+        });
+        res.status(201);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+});
+
+notes.delete('/:id', (req, res) => {
+    console.log('delete request received');
 });
 
 module.exports = notes;
